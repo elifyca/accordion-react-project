7e3db54c53b95aed022d4cf051a78b8c59235de3
Elif Yildiz <elifyca@gmail.com>
2022-05-24 11:33:44 -0700

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState} from "react"
+import data from "./data";
+import SingleQuestion from "./Ouestion ";
+import SingleOuestion from "./Ouestion ";
+
 
 function App() {
+  const [questions, SetQuestions]=useState(data);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <h3>questions and answer about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion  key={question.id} {...question}></SingleQuestion>
+            )
+          })}
+
+        </section>
+        </div>
+    </main>
   );
 }
 
